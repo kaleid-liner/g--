ef1190a1f8cb1af30aa40d392c78a8403b8fcafd
kalineid <noob@mail.ustc.edu.cn>
2019-11-25 19:29:28 +0800

    Add tests

---
+++ c1interpreter/test/assign_to_global.c
@@ -0,0 +1,7 @@
+int i1;
+int i2 = 0;
+
+void main() {
+    i1 = 1;
+    i2 = 1;
+}
---
+++ c1interpreter/test/branch.c
@@ -0,0 +1,15 @@
+void main() {
+    int a = 0;
+    int b;
+    if (a != 0) {
+        b = 0;
+    } else {
+        b = 1;
+    }
+    while (a < 3) {
+        a = a + 1;
+        output_ivar = b;
+        outputInt();
+    }
+}
+
